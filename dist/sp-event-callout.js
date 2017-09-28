11964b0f540dca9d71ba3193adb34325848c811b
Gunjan Datta <me@dattabase.com>
2017-09-28 18:48:02 -0400

    Built the project.

--- dist/sp-event-callout.js
+++ dist/sp-event-callout.js
@@ -189,7 +189,7 @@ var gd_sprest_1 = __webpack_require__(10);
 /**
  * SharePoint Calendar Event Callout Class
  */
-var SPEventCallout = (function () {
+var SPEventCallout = /** @class */ (function () {
     /**
      * Constructor
      */
@@ -210,58 +210,36 @@ var SPEventCallout = (function () {
         this._onItemsSucceed = null;
         // List Name
         this._listName = "";
-        // The hover out event
-        this.hoverOutEvent = function () {
-            // Get the callout
-            var callout = _this._callouts[_this._currentItemId];
-            if (callout) {
-                // Close the callout w/ animation
-                callout.close(true);
-            }
-            // Clear the current item id
-            _this._currentItemId = 0;
-        };
-        // The hover over event
-        this.hoverOverEvent = function (ev) {
-            // Get the item id for this event
-            var link = ev.currentTarget.querySelector("a");
-            var itemId = link ? link.href.substr(link.href.indexOf("ID=") + 3) : 0;
-            if (itemId > 0 && itemId != _this._currentItemId) {
-                // Set the current item id
-                _this._currentItemId = itemId;
-                // Get the callout
-                var callout_1 = _this._callouts[_this._currentItemId];
-                // Get the item
-                _this.getItemInfo(_this._currentItemId).then(function (item) {
-                    var content = "";
-                    // Get the content element
-                    var elContent = callout_1.getContentElement().querySelector(".js-callout-body");
-                    // Parse the fields to display
-                    for (var i = 0; i < _this._fields.length; i++) {
-                        var field = _this._fields[i];
-                        var title = field;
-                        var value = item[field];
-                        // See if this is a date/time field
-                        if (field == "EndDate" || field == "EventDate") {
-                            // Convert the date field
-                            value = (new Date(value)).toString();
-                            // Set the title
-                            title = field == "EndDate" ? "End Date" : "Start Date";
-                        }
-                        // Update the content
-                        content += "<div><strong>" + title + ": </strong>" + value + "</div>";
+        // Method to render the callout content
+        this.renderCalloutContent = function (callout, itemId) {
+            // Get the item
+            _this.getItemInfo(itemId).then(function (item) {
+                var content = "";
+                // Get the content element
+                var elContent = callout.getContentElement().querySelector(".js-callout-body");
+                // Parse the fields to display
+                for (var i = 0; i < _this._fields.length; i++) {
+                    var field = _this._fields[i];
+                    var title = field;
+                    var value = item[field];
+                    // See if this is a date/time field
+                    if (field == "EndDate" || field == "EventDate") {
+                        // Convert the date field
+                        value = (new Date(value)).toString();
+                        // Set the title
+                        title = field == "EndDate" ? "End Date" : "Start Date";
                     }
-                    // Update the content element
-                    elContent.innerHTML = content;
-                });
-                // Open the callout
-                callout_1.open();
-            }
+                    // Update the content
+                    content += "<div><strong>" + title + ": </strong>" + value + "</div>";
+                }
+                // Update the content element
+                elContent.innerHTML = content;
+            });
         };
         // Save the list name
         this._listName = listName;
-        // Ensure the callout library is loaded
-        SP.SOD.executeFunc("callout.js", "Callout", function () {
+        // Ensure the SP library is loaded
+        SP.SOD.loadMultiple(["callout.js", "sp.ui.dialog.js"], function () {
             // Wait for the calendar script to be loaded
             ExecuteOrDelayUntilScriptLoaded(function () {
                 var _this_ = _this;
@@ -283,28 +261,39 @@ var SPEventCallout = (function () {
      */
     // Method to attach callouts to the events
     SPEventCallout.prototype.attachCalloutsToEvents = function () {
+        var _this = this;
         // Clear the callouts
         this._callouts = [];
         // Parse the calendar events
         var calEvents = document.querySelectorAll(".ms-acal-item");
         for (var i = 0; i < calEvents.length; i++) {
             var calEvent = calEvents[i];
-            // Add hover events
-            calEvent.addEventListener("mouseover", this.hoverOverEvent);
-            calEvent.addEventListener("mouseout", this.hoverOutEvent);
             // Get the item id for this event
             var link = calEvent.querySelector("a");
             var itemId = link ? link.href.substr(link.href.indexOf("ID=") + 3) : 0;
-            // Create the callout options
-            var calloutOptions = new CalloutOptions();
-            calloutOptions.content = "<div>Loading the Event Information...</div>";
-            calloutOptions.ID = itemId;
-            calloutOptions.launchPoint = calEvent;
-            calloutOptions.title = calEvent.title;
-            // Remove the default hover text
-            calEvent.removeAttribute("title");
-            // Create the callout
-            this._callouts[itemId] = CalloutManager.createNew(calloutOptions);
+            if (itemId > 0) {
+                // Remove the default hover text
+                calEvent.removeAttribute("title");
+                // Get the callout
+                var callout = CalloutManager.getFromLaunchPointIfExists(calEvent);
+                if (callout == null) {
+                    // Create the callout
+                    callout = CalloutManager.createNewIfNecessary({
+                        beakOrientation: "leftRight",
+                        content: "<div>Loading the Event Information...</div>",
+                        ID: i + "_" + itemId,
+                        launchPoint: calEvent,
+                        openOptions: { event: "hover", showCloseButton: true },
+                        title: calEvent.title,
+                        onOpeningCallback: function (callout) {
+                            // Get the item id
+                            var itemId = callout.getID().split("_")[1];
+                            // Render the item
+                            _this.renderCalloutContent(callout, itemId);
+                        }
+                    });
+                }
+            }
         }
     };
     // Method to get the item Information
